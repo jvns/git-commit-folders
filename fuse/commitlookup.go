cb13219c53cc207c5a1052213b1bc3551c15facd
Julia Evans <julia@jvns.ca>
2023-11-30 12:08:51 -0500

    codecodecode

--- fuse/commitlookup.go
+++ fuse/commitlookup.go
@@ -1,12 +1,17 @@
 package fuse
 
 import (
+	"bytes"
 	"errors"
+	"strings"
 
+	"github.com/go-git/go-billy"
 	"github.com/go-git/go-billy/v5/helper/chroot"
 	"github.com/go-git/go-billy/v5/osfs"
 	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/format/idxfile"
+	"github.com/go-git/go-git/v5/plumbing/hash"
 	"github.com/go-git/go-git/v5/storage/filesystem"
 	"github.com/go-git/go-git/v5/storage/filesystem/dotgit"
 	"github.com/go-git/go-git/v5/utils/ioutil"
@@ -59,4 +64,89 @@ func getIndexes(repo *git.Repository) ([]*idxfile.MemoryIndex, error) {
 		indexes = append(indexes, idxf)
 	}
 	return indexes, nil
+}
+
+func initCache(repo *git.Repository) (*Cache, error) {
+	indexes, err := getIndexes(repo)
+	if err != nil {
+		return nil, err
+	}
+	return &Cache{indexes: indexes, repo: repo}, nil
+}
+
+type Cache struct {
+	indexes []*idxfile.MemoryIndex
+	repo    *git.Repository
+	fs      billy.Filesystem
+}
+
+func IndexHasPrefix(index *idxfile.MemoryIndex, hash plumbing.Hash) bool {
+	_, found := findHashIndex(index, hash)
+	return found
+}
+
+func LooseObjectsHasPrefix(fs billy.Filesystem, hash plumbing.Hash) bool {
+	sha := hash.String()
+	dir, err := fs.ReadDir(fs.Join("objects", sha[:2]))
+	if err != nil {
+		return false
+	}
+	for _, entry := range dir {
+		name := entry.Name()
+		if strings.HasPrefix(name, sha[2:]) {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *Cache) HasPrefix(prefix string) bool {
+	prefixHash := plumbing.NewHash(prefix)
+	for _, index := range c.indexes {
+		if IndexHasPrefix(index, prefixHash) {
+			return true
+		}
+	}
+	return LooseObjectsHasPrefix(c.fs, prefixHash)
+}
+
+/* copied from idxfile.go */
+func findHashIndex(idx *idxfile.MemoryIndex, h plumbing.Hash) (int, bool) {
+	objectIDLength := uint64(hash.Size)
+	noMapping := -1
+	k := idx.FanoutMapping[h[0]]
+	if k == noMapping {
+		return 0, false
+	}
+
+	if len(idx.Names) <= k {
+		return 0, false
+	}
+
+	data := idx.Names[k]
+	high := uint64(len(idx.Offset32[k])) >> 2
+	if high == 0 {
+		return 0, false
+	}
+
+	low := uint64(0)
+	for {
+		mid := (low + high) >> 1
+		offset := mid * objectIDLength
+
+		cmp := bytes.Compare(h[:], data[offset:offset+objectIDLength])
+		if cmp < 0 {
+			high = mid
+		} else if cmp == 0 {
+			return int(mid), true
+		} else {
+			low = mid + 1
+		}
+
+		if low >= high {
+			break
+		}
+	}
+
+	return 0, false
 }
